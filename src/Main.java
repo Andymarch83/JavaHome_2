c77771b6e0d8d9bf7482593ce74d0fb94c0b1101
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2023-06-12 23:10:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*
+Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+В итоге должно получится select * from students where name=Ivanov, country=Russia, city=Moscow, age=null
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String sql = "select * from students where ";
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        String json1=json.replace("{\"", "").replace("\"}", "")
+                .replace("\":\"", "=").replace("\", \"", ", ");
+        StringBuilder sq = new StringBuilder(sql);
+        System.out.println(sq.append(json1));
         }
     }
-}
